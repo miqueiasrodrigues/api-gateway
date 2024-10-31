97a128d80f584aaa9669ab9ceff9689684863a31
miqueiasrodrigues <santos.miqueias24@gmail.com>
2024-10-31 08:36:46 -0300

    feat: ajuste no gateway

--- src/main/java/com/miqueias/r/api_gateway/config/ApiGatewayConfig.java
+++ src/main/java/com/miqueias/r/api_gateway/config/ApiGatewayConfig.java
@@ -1,8 +1,21 @@
 package com.miqueias.r.api_gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfig {
-
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/usuario-servico/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://usuario-servico"))
+                .route(p -> p.path("/autenticacao-servico/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://autenticacao-servico"))
+                .build();
+    }
 }
